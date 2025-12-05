5e32cc7e23528757bd3e2b09496a6495b80c0a37
ilyas <ilyasichev2015@gmail.com>
2025-12-05 09:39:09 +0300

    change to record because of lack of empty constructors and setters (jackson message converter failes)

--- src/main/java/com/example/events_contract/events/DelicacyDeletedEvent.java
+++ src/main/java/com/example/events_contract/events/DelicacyDeletedEvent.java
@@ -2,14 +2,7 @@ package com.example.events_contract.events;
 
 import java.io.Serializable;
 
-public class DelicacyDeletedEvent implements Serializable {
-    private Long delicacyId;
-
-    public DelicacyDeletedEvent(Long delicacyId) {
-        this.delicacyId = delicacyId;
-    }
-
-    public Long getDelicacyId() {
-        return delicacyId;
-    }
+public record DelicacyDeletedEvent(
+        Long delicacyId
+) implements Serializable {
 }
--- src/main/java/com/example/events_contract/events/ProductDto.java
+++ src/main/java/com/example/events_contract/events/ProductDto.java
@@ -2,27 +2,9 @@ package com.example.events_contract.events;
 
 import java.io.Serializable;
 
-public class ProductDto implements Serializable {
-
-    private Long id;
-    private String name;
-    private Double price;
-
-    public ProductDto(Long id, String name, Double price) {
-        this.id = id;
-        this.name = name;
-        this.price = price;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public Double getPrice() {
-        return price;
-    }
+public record ProductDto(
+        Long id,
+        String name,
+        Double price
+) implements Serializable {
 }
